171acdd2a04a3723b0ebbf4b968ebfcb5db4ac0d
Jean <jean_luccalp@hotmail.com>
2025-03-12 17:51:00 +0100

    branch 03 - criando a página portfólio e javascript

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+
+// setTimeout(() => {
+//     window.location.href = "index.html";
+//   }, 5000); // Redireciona em 5  
+
+
+
+// Define o timeout
+// let timer = setTimeout(() => {
+//     window.location.href = "index.html";
+//    }, 5000);
+
+//   // Cancela o timeout
+//   clearTimeout(timer);
+
+// Verifica a URL atual
+// if (window.location.pathname !== "/index.html") {
+//     // Redireciona para a página inicial
+//     window.location.href = "index.html";
+// }
+
+// Verifica se já foi redirecionado para a home
+// let jaRedirecionado = sessionStorage.getItem("jaRedirecionado"); // Pega o valor da variável
+// // Verifica se já foi redirecionado para a home
+// if (!sessionStorage.getItem("jaRedirecionado")) {
+//     // Marca como redirecionado e volta para a página inicial
+//     sessionStorage.setItem("jaRedirecionado", "true");
+//     if (window.location.pathname !== "index.html") {
+//       window.location.href = "index.html";
+//     }
+// }
